d3f08b65b60343d38d0ec762edc1c1a3fe0bdac8
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-23 01:02:31 +0800

    Finished the firebase deployment set-up.

--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,1 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+(()=>{"use strict";M.AutoInit()})();
