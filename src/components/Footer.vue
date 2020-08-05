04f358996333371c9f641524d17d21061edfeb26
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-05 23:33:58 +0800

    Added correct links.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,33 +24,36 @@
         <div class="w-full lg:w-6/12 px-4">
           <h4 class="text-2xl font-semibold">Let's keep in touch!</h4>
           <div class="mt-6">
-            <button
-              class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-              type="button"
-            >
-              <i class="flex fab fa-twitter"></i></button
-            ><button
-              class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-              type="button"
-            >
-              <i class="flex fab fa-facebook-square"></i></button
-            ><button
-              class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-              type="button"
-            >
-              <i class="flex fab fa-dribbble"></i></button
-            ><button
-              class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
-              type="button"
-            >
-              <i class="flex fab fa-github"></i>
-            </button>
+            <a href="https://twitter.com/Ariel_Magbanua">
+              <button
+                class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i class="flex fab fa-twitter"></i>
+              </button>
+            </a>
+            <a href="https://www.facebook.com/ariel.magbanua">
+              <button
+                class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i class="flex fab fa-facebook-square"></i>
+              </button>
+            </a>
+            <a href="https://github.com/arielmagbanua">
+              <button
+                class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i class="flex fab fa-github"></i>
+              </button>
+            </a>
           </div>
         </div>
         <div class="w-full lg:w-6/12 px-4">
           <div class="flex flex-wrap items-top mb-6">
             <div class="w-full lg:w-12/12 px-4 ml-auto">
-              <h4 class="text-2xl font-semibold">Useful Links</h4>
+              <h4 class="text-2xl font-semibold">Site Links</h4>
               <ul class="list-unstyled">
                 <li>
                   <a class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm" href="/me">
