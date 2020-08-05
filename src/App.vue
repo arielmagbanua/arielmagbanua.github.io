d2bbe8679c4c120fd48e281b777e4fbe1f599c48
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-05 23:32:40 +0800

    Replaced the main section with router view.

--- src/App.vue
+++ src/App.vue
@@ -26,55 +26,8 @@
         </div>
       </section>
       <section class="relative py-16 bg-gray-300">
-        <div class="container mx-auto px-4">
-          <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-            <div class="px-6">
-              <div class="flex flex-wrap justify-center">
-                <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                  <div class="relative">
-                    <img
-                      alt="..."
-                      src="./assets/img/pic.jpg"
-                      class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                      style="max-width: 150px;"
-                    />
-                  </div>
-                </div>
-                <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"></div>
-                <div class="w-full lg:w-4/12 px-4 lg:order-1"></div>
-              </div>
-              <div class="text-center mt-24">
-                <h3 class="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
-                  Ariel Magbanua
-                </h3>
-                <div class="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-                  <i class="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>
-                  Davao City, Philippines
-                </div>
-                <div class="mb-2 text-gray-700 mt-10">
-                  <i class="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
-                  Software Engineer - Research and Development - Zeald
-                </div>
-              </div>
-              <div class="mt-10 py-10 border-t border-gray-300 text-center">
-                <div class="flex flex-wrap justify-center">
-                  <div class="w-full lg:w-9/12 px-4">
-                    <p class="mb-4 text-lg leading-relaxed text-gray-800">
-                      I am currently serving as a senior-level software engineer in the Research and Development Department.
-                      Our department focuses on research and development of our current or upcoming systems, products, and tools.
-
-                      I'm always enthusiastic to explore new programming languages, technologies, new ideas, and practices for mobile and web.
-                      I always look forward to building or engineering cool and useful applications for people to use.
-                    </p>
-                    <a href="#pablo" class="font-normal text-pink-500"
-                      >Show more</a
-                    >
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <!-- Route Section -->
+        <router-view></router-view>
       </section>
     </main>
     <footer-component></footer-component>
