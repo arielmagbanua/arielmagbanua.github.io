f4f3b2b1c234c2ba84db11f2823f69f7a5df36b4
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-05 23:38:57 +0800

    Added Projects component.

---
+++ src/views/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container mx-auto px-4 profile">
+    <div
+      class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
+    >
+      <div class="px-6">
+        <div class="mt-10 py-10 text-center">
+          <div class="flex flex-wrap justify-center">
+            <div class="w-full lg:w-9/12 px-4">
+              <p class="mb-4 text-lg leading-relaxed text-gray-800">
+                Coming Soon!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import WorkCard from '../components/WorkCard.vue'
+
+export default {
+  components: {
+    // WorkCard
+  }
+}
+</script>
