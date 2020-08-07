d88e22e3c0f4f0afe73935fee0dc60982045ba7a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-07 22:12:02 +0800

    Added text rendering to other technologies that don't have available icons.

--- src/components/TechLabel.vue
+++ src/components/TechLabel.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="inline-block mx-2 h-auto flex-grow">
-    <i :class="techClass"></i>
+    <i v-if="renderable" :class="techClass"></i>
+    <span
+      v-else
+      class="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+      {{ lang }}
+    </span>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      renderableTech: [
+        'php',
+        'laravel',
+        'html5',
+        'css3',
+        'vuejs',
+        'js',
+        'node-js',
+        'google-play',
+        'java',
+        'android'
+      ]
+    }
+  },
   props: {
     lang: {
       type: String,
@@ -15,6 +36,9 @@ export default {
   computed: {
     techClass () {
       return `fab fa-${this.lang} fa-2x`
+    },
+    renderable () {
+      return this.renderableTech.includes(this.lang)
     }
   }
 }
