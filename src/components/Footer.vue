ea176b3c00f6f869afd0f1e16987b14a7dade2fb
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-08 17:12:47 +0800

    Improved the footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="relative bg-gray-300 pt-8 pb-6">
+  <footer class="relative bg-gray-300 pt-6 pb-6">
     <div
       class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
       style="height: 80px; transform: translateZ(0px);"
@@ -16,13 +16,17 @@
         <polygon
           class="text-gray-300 fill-current"
           points="2560 0 2560 100 0 100"
-        ></polygon>
+        >
+        </polygon>
       </svg>
     </div>
     <div class="container mx-auto px-4">
       <div class="flex flex-wrap">
-        <div class="w-full lg:w-6/12 px-4">
-          <h4 class="text-2xl font-semibold">Let's keep in touch!</h4>
+        <div class="w-full lg:w-6/12 p-4">
+          <h4 class="text-3xl font-semibold">Let's keep in touch!</h4>
+          <h5 class="text-lg mt-0 mb-2 text-gray-700">
+            Find me on any of these platforms.
+          </h5>
           <div class="mt-6">
             <a href="https://twitter.com/Ariel_Magbanua">
               <button
@@ -48,25 +52,110 @@
                 <i class="flex fab fa-github"></i>
               </button>
             </a>
+            <a href="https://www.linkedin.com/in/arielmagbanua/">
+              <button
+                class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i class="flex fab fa-linkedin"></i>
+              </button>
+            </a>
+            <a href="http://www.youtube.com/c/ArielMagbanua">
+              <button
+                class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                type="button"
+              >
+                <i class="flex fab fa-youtube"></i>
+              </button>
+            </a>
           </div>
         </div>
-        <div class="w-full lg:w-6/12 px-4">
-          <div class="flex flex-wrap items-top mb-6">
-            <div class="w-full lg:w-12/12 px-4 ml-auto">
-              <h4 class="text-2xl font-semibold">Site Links</h4>
+        <div class="w-full lg:w-6/12 p-4">
+          <div class="flex flex-wrap mb-6">
+            <div class="w-full px-4">
+              <span
+                class="block uppercase text-gray-600 text-sm font-semibold mb-2"
+              >
+                Useful Links
+              </span>
               <ul class="list-unstyled">
                 <li>
-                  <a class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm" href="/me">
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="/"
+                  >
                     About Me
                   </a>
                 </li>
                 <li>
-                  <a class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm" href="/projects">
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="/projects"
+                  >
                     Projects
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="https://github.com/arielmagbanua"
+                  >
+                    Github
                   </a>
                 </li>
               </ul>
             </div>
+            <!-- <div class="w-full lg:w-4/12 px-4">
+              <span
+                class="block uppercase text-gray-600 text-sm font-semibold mb-2"
+              >
+                Other Resources
+              </span>
+              <ul class="list-unstyled">
+                <li>
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
+                  >
+                    MIT License
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="https://creative-tim.com/terms"
+                  >
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="https://creative-tim.com/privacy"
+                  >
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                    href="https://creative-tim.com/contact-us"
+                  >
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <hr class="my-6 border-gray-400" />
+      <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
+      >
+        <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+          <div class="text-sm text-gray-600 font-semibold py-1">
+            Copyright © {{date}} | Ariel Magbanua
           </div>
         </div>
       </div>
