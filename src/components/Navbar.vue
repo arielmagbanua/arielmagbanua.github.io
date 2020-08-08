10819e69bcfbe9317799122a1fc23585ea92de3c
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-08 17:22:05 +0800

    Added on hover nav links.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,10 @@
   <nav class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
       <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white hover:text-gray-400">
           <router-link to="/">Me</router-link>
         </a>
-        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white hover:text-gray-400">
           <router-link to="/projects">Projects</router-link>
         </a>
         <button
