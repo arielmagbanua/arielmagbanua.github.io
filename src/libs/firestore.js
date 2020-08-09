0a2b5e27fd6ba9103ff21a749b6f76fc3a937673
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-09 20:52:15 +0800

    Added configuration for firestore.

--- src/libs/firestore.js
+++ src/libs/firestore.js
@@ -1,19 +1,17 @@
 const firebase = require('firebase')
 // Required for side-effects
 require('firebase/firestore')
-// configure dotenv
-require('dotenv').config()
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyAgvbmOg2eHeAN1TXF85p4l2yEkIkjQ2Bk',
+  authDomain: 'arielmagbanua.firebaseapp.com',
+  databaseURL: 'https://arielmagbanua.firebaseio.com',
+  projectId: 'arielmagbanua',
+  storageBucket: 'arielmagbanua.appspot.com',
+  messagingSenderId: '40927591444',
+  appId: '1:40927591444:web:e5b68237bd554c32947b01',
+  measurementId: 'G-J9DDBCR3Z0'
 })
 
 const firestore = firebase.firestore()
