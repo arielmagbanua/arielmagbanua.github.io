98e0e7e0a00970694ef3c878740575c0d4e8d288
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-08-09 20:52:43 +0800

    Code clean up.

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -22,56 +22,25 @@
 
 <script>
 import ProjectCard from '../components/ProjectCard.vue'
+import { firestore } from '../libs/firestore'
 
 export default {
   data () {
     return {
-      projects: [
-        {
-          title: 'Personal Page',
-          description: 'My personal profile page which details my current work and experience as well as my projects.',
-          cover: 'https://images.unsplash.com/photo-1507494924047-60b8ee826ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1932&q=80',
-          url: 'https://arielmagbanua.com/',
-          technologies: [
-            'html5', 'css3', 'js', 'vuejs', 'firebase hosting'
-          ]
-        },
-        {
-          title: 'Ariel and Phebe Wedding',
-          description: 'The official wedding page of Ariel and Phebe. It has submit rsvp feature and map to where the wedding and reception will take place.',
-          cover: 'https://arielandphebe.com/img/hero/hero4.jpg',
-          url: 'https://arielandphebe.com/',
-          technologies: [
-            'html5', 'css3', 'js', 'vuejs', 'node-js', 'firestore', 'firebase functions', 'twitter api', 'google cloud vision'
-          ]
-        },
-        {
-          title: 'Para Po!',
-          description: 'It allows the user to find jeepney that passes to his/her desired origin and destination locations. It does compute estimated fare for each possible jeepney for those who are not familiar with the usual fare of Davao City jeepneys.',
-          technologies: [
-            'android', 'java', 'google-play', 'fcm', 'google maps', 'places api'
-          ]
-        },
-        {
-          title: 'Para Po! (Web)',
-          description: 'The web app allows the user to search and view all available Jeepney routes in Davao City. The web application also serves as administrator and data source of the partner android application which is Para Po!',
-          technologies: [
-            'laravel', 'php', 'html5', 'css3', 'mysql', 'google maps'
-          ]
-        },
-        {
-          title: 'Lead Reactor',
-          description: 'Web application for managing the Engage Logic IQ publishers, advertisers, survey campaigns, and collection of leads.',
-          url: 'http://leadreactor.engageiq.com/',
-          technologies: [
-            'laravel', 'php', 'html5', 'css3', 'js', 'mysql'
-          ]
-        }
-      ]
+      projects: []
     }
   },
   components: {
     ProjectCard
+  },
+  mounted: async function () {
+    const snapshot = await firestore.collection('projects')
+      // .where('show', '==', true) // TODO: Fix the where clause
+      .orderBy('importance', 'desc')
+      .get()
+
+    this.projects = snapshot.docs.map((doc) => doc.data())
+      .filter((project) => project.show)
   }
 }
 </script>
