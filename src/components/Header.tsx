4172224ad81e6e74ea3ff2414b88fb49ec9b4277
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-09-24 21:19:42 +0800

    Finished the styles of header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,55 @@
 import classNames from 'classnames';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { MdDarkMode, MdMenu } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
-  const [navOpen, setNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-  const verticalNavRef = useRef<HTMLDivElement>(null);
-  const headerRef = useRef<HTMLHeadElement>(null);
+  const headerClasses = classNames(
+    'flex flex-col items-stretch justify-center py-4 max-h-20 bg-surface-dim dark:bg-surface-dim-dark text-on-surface dark:text-on-surface-dark shadow-sm overflow-hidden transition-all duration-500',
+    'xl:px-20',
+    'lg:px-20',
+    'md:px-20',
+    'sm:px-8',
+    'xs:px-8',
+    '2xs:px-8',
+    {
+      'max-h-48': navOpen,
+    }
+  );
 
   const verticalNavigationClasses = classNames(
     'flex-col items-stretch justify-stretch space-y-2 text-lg tracking-wide h-0 overflow-hidden',
-    'md:flex sm:flex xs:flex 2xs:flex',
-    'transition-all duration-500'
+    'xl:hidden',
+    'lg:hidden',
+    'md:flex',
+    'sm:flex',
+    'xs:flex',
+    '2xs:flex',
+    'transition-all duration-500',
+    {
+      'h-36': navOpen,
+    }
   );
 
   const toggleNavDropdown = () => {
-    const header = headerRef.current;
-    if (header) {
-      if (header.classList.contains('max-h-20')) {
-        header.classList.remove('max-h-20');
-        header.classList.add('max-h-48');
-      } else {
-        header.classList.remove('max-h-48');
-        header.classList.add('max-h-20');
-      }
-    }
+    setNavOpen(!navOpen);
+  };
 
-    const verticalNav = verticalNavRef.current;
+  const navigateHandleClick = (to: string) => {
+    navigate(`/${to}`);
+    setNavOpen(false);
+  };
 
-    if (verticalNav) {
-      if (verticalNav.classList.contains('h-32')) {
-        verticalNav.classList.remove('h-32');
-        verticalNav.classList.add('h-0');
-      } else {
-        verticalNav.classList.remove('h-0');
-        verticalNav.classList.add('h-32');
-      }
-    }
+  const handleThemeClick = () => {
+    // TODO: change the theme here
+    setNavOpen(false);
   };
 
   return (
-    <header
-      ref={headerRef}
-      className="flex flex-col items-stretch justify-center py-4 xl:px-20 lg:px-20 md:px-20 sm:px-8 xs:px-8 2xs:px-8 max-h-20 bg-surface-container dark:bg-surface-container-dark text-on-surface dark:text-on-surface-dark shadow-sm overflow-hidden transition-all duration-500"
-    >
+    <header className={headerClasses}>
       <div className="flex items-center justify-between py-4">
         <div className="flex items-baseline">
           <h1 className="text-4xl 2xl:text-4xl xl:text-4xl lg:text-4xl md:text-4xl sm:text-2xl xs:text-2xl 2xs:text-2xl tracking-wider hover:tracking-wide hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark cursor-pointer transition-all duration-500">
@@ -56,7 +61,7 @@ export default function Header() {
         <div className="text-right w-100 flex items-center justify-end 2xl:hidden xl:hidden lg:hidden md:visible sm:visible xs:visible 2xs:visible">
           <MdMenu
             onClick={toggleNavDropdown}
-            className="text-3xl cursor-pointer"
+            className="text-2xl cursor-pointer"
           />
         </div>
         <nav className="flex items-center justify-end space-x-4 text-lg tracking-wide 2xl:flex xl:flex lg:flex md:hidden sm:hidden xs:hidden 2xs:hidden">
@@ -72,23 +77,26 @@ export default function Header() {
           >
             Portfolio
           </Link>
-          <MdDarkMode className="cursor-pointer hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark" />
+          <MdDarkMode className="hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark cursor-pointer" />
         </nav>
       </div>
-      <nav ref={verticalNavRef} className={verticalNavigationClasses}>
-        <Link
-          className="hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark"
-          to="/about"
+      <nav className={verticalNavigationClasses}>
+        <p
+          onClick={() => navigateHandleClick('about')}
+          className="hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark cursor-pointer"
         >
           About
-        </Link>
-        <Link
-          className="hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark"
-          to="/portfolio"
+        </p>
+        <p
+          onClick={() => navigateHandleClick('portfolio')}
+          className="hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark cursor-pointer"
         >
           Portfolio
-        </Link>
-        <MdDarkMode className="cursor-pointer hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark" />
+        </p>
+        <MdDarkMode
+          onClick={handleThemeClick}
+          className="hover:text-on-surface-variant dark:hover:text-on-surface-variant-dark cursor-pointer"
+        />
       </nav>
     </header>
   );
